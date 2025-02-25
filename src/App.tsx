23976ccf98b065a31d4ca69c3307cf5cacfbc0bc
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-25 22:16:48 +0100

    some basic css changes added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState<string>('')
+
+  const onUse = async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    if (tab?.id) {
+      chrome.scripting.executeScript<string[], void>({
+        target: { tabId: tab.id },
+        args: [color],
+        func: (color) => {
+          // Change text color for within body only
+          // document.body.style.color = color
+
+          // Change text color on whole website
+          document.querySelectorAll('*').forEach((el) => {
+            ;(el as HTMLElement).style.color = color
+          })
+
+          // Set the background color for body
+          // document.body.style.backgroundColor = 'black'
+
+          // Set the background color for all elements
+          document.querySelectorAll('*').forEach((el) => {
+            ;(el as HTMLElement).style.backgroundColor = 'black'
+          })
+
+          // alert('Hello from app!')
+        },
+      })
+    } else {
+      console.error('No active tab found')
+    }
+  }
 
   return (
     <>
@@ -18,9 +50,19 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input
+          type="color"
+          onChange={(e) => setColor(e.currentTarget.value)}
+          value={color}
+        />
+
+        <button
+          className="p-2 border-2 border-blue-600 rounded-xl bg-purple-900 hover:cursor-pointer font-bold"
+          onClick={onUse}
+        >
+          Button
         </button>
+
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
