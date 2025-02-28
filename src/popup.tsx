7d39267328ed2052a5739f374e7fc766dc94fba9
Patryk Wargacki <wargacki.patryq@gmail.com>
2025-02-28 14:21:52 +0100

    popup color changer button fix

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,4 +1,4 @@
-import { ColorChanger } from '~features/color-changer'
+import { ColorChangerPopup } from '~features/color-changer-popup'
 import { CountButton } from '~features/count-button'
 
 import '~style.css'
@@ -7,7 +7,7 @@ function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-flex-col plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
       <CountButton />
-      <ColorChanger />
+      <ColorChangerPopup />
     </div>
   )
 }
